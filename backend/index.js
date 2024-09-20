8812ca64925ffc0e31cc2c4f4af434d3306eb218
Sakar <sk4sakar@gmail.com>
2024-09-20 20:27:57 +0530

    frontend url updates

--- backend/index.js
+++ backend/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "https://synapse-ai.onrender.com/",
+		origin: "https://synapse-ai.onrender.com",
 		credentials: true,
 	})
 )
